da590c7a39982aff33a1c14b5dda10bdfc0d7f2c
Surender Rawat <surenderrawat@197NODMB27919.local>
2021-04-23 18:13:31 +0530

    Adding ingress rule

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -4,12 +4,19 @@ package com.dockerforjavadevelopers.hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index() {
-        return "Hello World\n";
+        Date dNow = new Date( );
+        SimpleDateFormat ft =
+                new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+        return "Welcome to world of EKS auto deployment. Current Date: "+ft.format(dNow);
     }
     
 }
